329f1ec1aa7e83454acb3fc1966add3bae085837
brian <aeoliun@gmail.com>
2020-11-04 11:02:38 -0500

    initial event creation

---
+++ scripts/EventForm.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Socket } from "./Socket"
+
+export default function EventForm() {
+
+    let eventTypeReference = React.useRef();
+    let locationReference = React.useRef();
+    let timeReference = React.useRef();
+
+    function sendNewEvent(e) {
+        console.log("Sending new event")
+        console.log(eventTypeReference.current.value)
+        console.log(locationReference.current.value)
+        console.log(timeReference.current.value)
+
+        Socket.emit("sending new event", {
+            "type": eventTypeReference.current.value,
+            "location": locationReference.current.value,
+            "time": timeReference.current.value
+        })
+
+        console.log("Sent new event")
+        e.preventDefault()
+    }
+
+    return (
+        <div>
+            <form onSubmit={sendNewEvent}>
+                <select name="event-type" ref={ eventTypeReference }>
+                    <option value="Study">Study</option>
+                    <option value="Hangout">Hangout</option>
+                </select>
+                <input type="text" placeholder="Enter a location..." ref={ locationReference }/>
+                <input placeholder="12:00 PM" type="time" min="00:00" max="23:59" ref={ timeReference }/>
+                <button type="submit-button">Submit</button>
+            </form>
+        </div>
+    )
+}
---
+++ scripts/EventSession.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function EventSession() {
+    return (
+        <div>
+            <p>Event Session</p>
+            <p>Location: </p>
+            <p>Time: </p>
+        </div>
+    )
+}
