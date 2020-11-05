0be3acea68541ed1f6b7a52934b3efe1e5bfbce9
brian <aeoliun@gmail.com>
2020-11-05 13:06:40 -0500

    event history basic functionality

--- scripts/EventForm.jsx
+++ scripts/EventForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import * as React from 'react';
 import { Socket } from "./Socket"
 
 export default function EventForm() {
--- scripts/EventHistory.jsx
+++ scripts/EventHistory.jsx
@@ -1,22 +1,39 @@
-import React from 'react'
+import * as React from 'react';
 import { Socket } from './Socket';
+import EventSession from "./EventSession"
 
 export default function EventHistory() {
-    const [events, setEvents] = React.useState([{}]);
+    const [events, setEvents] = React.useState([
+        {
+            "type": "Study",
+            "location": "Library"
+        },
+        {
+            "type": "Hangout",
+            "location": "Ramen Gami Rip"
+        }]);
 
     function getEventHistory() {
-        Socket.on("emit all events", updateEventHistory)
+        Socket.on("emit all events", updateEventHistory);
+        return () => {
+            Socket.off("emit all events", updateEventHistory)
+        }
     }
 
     function updateEventHistory(data) {
         console.log("Received event " + data["location"])
+        console.log(events);
     }
 
     getEventHistory();
 
     return (
         <div>
-
+            { events.map((event, index) => (
+                <EventSession key={index} type={ event.type } location={ event.location } />
+                // console.log(event.location)
+                // <p>{event.type} {event.location}</p>
+            ))}
         </div>
     )
 }
