725117e64432c7a7b5030dec0c71dc118541c77e
Luis Alf. Chiang <lhc4@njit.edu>
2020-11-07 18:49:13 -0400

    profile page close button

--- scripts/ProfilePage/Profile.jsx
+++ scripts/ProfilePage/Profile.jsx
@@ -6,6 +6,7 @@ export function Profile({ user, onClose }) {
     return (
     <div id={user.email} className="profile-overlay" onClick={onClose}>
         <div className="profile-container">
+            <button onClick={onClose} className="close-btn">&#10006;</button>
             <img src={user.profilePicture} alt="Profile Picture"/>
             <div className="profile-info">
                 <p>Name: {user.name}</p>
