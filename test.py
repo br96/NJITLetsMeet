3509a7b61e21cb5a0de950c31184fa3c56e89c77
EC2 Default User <ec2-user@ip-172-31-28-85.ec2.internal>
2020-11-08 18:01:13 +0000

    Add Tests

--- test.py
+++ test.py
@@ -6,5 +6,17 @@ class TestApp(unittest.TestCase):
     def test_example(self):
         pass
 
+class LocationResponse:
+    def __init__(self, location):
+        self.location = location
+
+class TimeResponse:
+    def __init__(self, time):
+        self.time = time
+
+class DescriptionResponse:
+    def __init__(self, description):
+        self.description = description
+
 if __name__ == "__main__":
     unittest.main()
