04ab9ca4517df496136bbc1d0261369bdb67d7be
jun93lee <ec2-user@ip-172-31-28-85.ec2.internal>
2020-11-08 19:21:50 +0000

    Edit Id Column

--- models.py
+++ models.py
@@ -16,7 +16,6 @@ class EventClass(db.Model):
 class User(db.Model):
     __tablename__ = "users"
     
-    id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(1000), primary_key=True)
     name = db.Column(db.String(128), nullable=False)
     bio = db.Column(db.String(1024))
